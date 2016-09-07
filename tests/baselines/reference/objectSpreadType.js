f50d954f8572a1f7acf834f605fb1e79b722eaa1
Nathan Shively-Sanders <nathansa@microsoft.com>
2016-09-07 09:16:55 -0700

    Add declaration emit to objectSpreadType test

--- tests/baselines/reference/objectSpreadType.js
+++ tests/baselines/reference/objectSpreadType.js
@@ -1,6 +1,28 @@
 //// [objectSpreadType.ts]
-var spreads: { id: string, ...T, ...U };
+function f<T,U>(t: T, u: U): { id: string, ...T, ...U } {
+    var spreads: { id: string, ...T, ...U } = { id: '1', ...t, ...u };
+    return spreads;
+}
 
 
 //// [objectSpreadType.js]
-var spreads;
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+function f(t, u) {
+    var spreads = __assign({id: '1'}, t, u);
+    return spreads;
+}
+
+
+//// [objectSpreadType.d.ts]
+declare function f<T, U>(t: T, u: U): {
+    id: string;
+    ...T;
+    ...U;
+};
--- tests/cases/conformance/es6/destructuring/objectSpreadType.ts
+++ tests/cases/conformance/es6/destructuring/objectSpreadType.ts
@@ -1,1 +1,5 @@
-var spreads: { id: string, ...T, ...U };
+// @declaration: true
+function f<T,U>(t: T, u: U): { id: string, ...T, ...U } {
+    var spreads: { id: string, ...T, ...U } = { id: '1', ...t, ...u };
+    return spreads;
+}
